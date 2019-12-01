83bb7ee5688b16431cb45f973c7b27e31903a62b
alsharifafnan <alsharifafnan20@gmail.com>
2019-12-01 17:24:39 +0300

    fades in

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("#clicker").fadeIn();
+      $("#secret").fadeIn("slow");
+      $("div").fadeIn(1000);
+    
+    });
+});
+
+
